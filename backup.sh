2ed09015bd68d8333d89202db10160f1839753e0
ricwi <richard.widen@mimer.com>
2021-10-28 11:54:17 +0200

    Test push to github actions

--- backup.sh
+++ backup.sh
@@ -3,12 +3,12 @@
 test -z $DEBUG || set -x
 
 dateISO() {
-  date -j -f "%s" $started -u  +"%Y-%m-%dT%H:%M:%SZ"
+  date -j -f "%s" $started -u  +"%Y%m%dT%H%M%SZ"
 }
 
 runbackup() {
   started=$(date +%s)
-  startedAt=$(date -u -d @$started  +"%Y-%m-%dT%H:%M:%SZ")
+  startedAt=$(date -u -d @$started  +"%Y%m%dT%H%M%SZ")
   if [ -n "${S3_ENDPOINT}" ]; then
     AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
   else
