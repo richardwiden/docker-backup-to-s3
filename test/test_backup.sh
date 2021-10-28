95fedc782d6a3f2f3fbc0e6f257a67413debebaa
ricwi <richard.widen@mimer.com>
2021-10-28 14:42:33 +0200

    Merge build and push

--- test/test_backup.sh
+++ test/test_backup.sh
@@ -16,5 +16,8 @@ docker rm helper
 docker build --tag docker-backup-to-s3:latest .
 docker run --rm --network local --name s3 -d -p 9000:9000 -e USER=myawskey -e PASSWORD=myawssecret altmannmarcelo/minio:latest
 docker run --rm --network local --name aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_EC2_METADATA_DISABLED amazon/aws-cli --endpoint-url http://s3:9000 s3 mb s3://my-bucket
-echo "VERSION=$(docker run --rm --network local --name backup -v data:/data -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e S3_PATH -e S3_ENDPOINT -e AES_PASSPHRASE docker-backup-to-s3:latest backup-once)" >> $GITHUB_ENV
+VERSION="$(docker run --rm --network local --name backup -v data:/data -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e S3_PATH -e S3_ENDPOINT -e AES_PASSPHRASE docker-backup-to-s3:latest backup-once)"
+export VERSION
+echo "VERSION=$VERSION"
+echo "VERSION=$VERSION"  >> $GITHUB_ENV
 docker volume rm data
