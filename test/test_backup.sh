d84324041e2f2be0a1a5741b938033209d1b3005
ricwi <richard.widen@mimer.com>
2021-10-28 14:23:07 +0200

    Change context

--- test/test_backup.sh
+++ test/test_backup.sh
@@ -13,7 +13,7 @@ echo "my test data" > test_input
 docker cp test_input helper:/data
 docker rm helper
 
-docker build --tag docker-backup-to-s3:latest  docker-backup-to-s3
+docker build --tag docker-backup-to-s3:latest .
 docker run --rm --network local --name s3 -d -p 9000:9000 -e USER=myawskey -e PASSWORD=myawssecret altmannmarcelo/minio:latest
 docker run --rm --network local --name aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_EC2_METADATA_DISABLED amazon/aws-cli --endpoint-url http://s3:9000 s3 mb s3://my-bucket
 echo "VERSION=$(docker run --rm --network local --name backup -v data:/data -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e S3_PATH -e S3_ENDPOINT -e AES_PASSPHRASE docker-backup-to-s3:latest backup-once)" >> $GITHUB_ENV
